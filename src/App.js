768bdf6346976866cd9754ce7eb97f75ccd8330f
Mukesh buldak <mukeshkumar.buldak12@gmail.com>
2021-08-10 00:36:12 +0530

    pushing to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+
+  const [cartIsShown, setCartIsShowm] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShowm(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShowm(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}></Header>
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
